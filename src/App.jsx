5a8bf259e756e4714d2f18aa1137a7dee06cf69e
ctsan2008 <ctsan2008@gmail.com>
2024-03-28 21:33:37 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import Counter from "./Counter";
+// import OnChange from "./OnChange";
+// import ColorPicker from "./ColorPicker";
+// import CounterUpdater from "./CounterUpdater";
+// import UpdateArray from "./UpdateArray";
+// import UpdateArrayOfObjects from "./UpdateArrayOfObjects";
+// import ToDoList from "./ToDoList";
+// import DigitalClock from "./DigitalClock";
+import Stopwatch from "./Stopwatch";
+
+function App() {
+  return <Stopwatch />;
+}
+
+export default App;
